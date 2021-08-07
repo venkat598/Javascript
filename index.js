1a08b64fddeba724a8a18caea931e8c38c713d75
venkat598 <58764451+venkat598@users.noreply.github.com>
2021-08-07 09:32:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,89 @@
+    // Javascript console API
+    //console.log("hello world");
+    //console.warn("fuck you"); 
+    //console.clear();
+    //alert("me");//
+
+    // 3. JavaScript Variables
+
+    // var number1 = 34;
+    // var number2 = 56;
+    // console.log(number1 + number2); 
+
+// var a = true;
+// var b = false;
+// console.log(a, b);
+
+// var und = undefined;
+// console.log(und);
+
+// var n = null;
+// console.log(n);
+
+
+
+// var arr = [1,2,3,4,5]
+// console.log(true && true);
+// console.log(true && false);
+// console.log(false && true);
+// console.log(false  false);
+
+function avg(a, b){
+    return (a + b)/2;
+}
+
+c1 = avg(4, 6);
+c2 = avg(14, 16);
+
+// console.log(c1, c2); 
+
+// console.log('3')
+
+var age = 34;
+if(age>8){
+    console.log('you are not a kid');
+}
+else{
+console.log('you are a kid');
+
+}
+var arr = [1,2,3,4,5,6,7];
+console.log(arr);
+for(var i=0;i<arr.length;i++)
+// if(i==2){
+    // break;
+//continue
+//}
+{
+console.log(arr[i])
+
+}
+arr.forEach(function(element){
+    console.log(element)
+})
+
+//Let j=0;
+while(j<arr.length){
+console.log(arr[j]);
+j ++;
+
+}
+do
+ {
+console.log(arr[j]);
+j++;
+
+}
+while(j<arr.length);
+
+//Let myArr = ["Fan", "Camera" , 34 , null, true];
+//console.log(myArr.length)
+//myArr.pop();
+//console.log(myArr);
+//const newLen
+//console.log()
+
+//Let elem = document.getElementById('click') 
+//{
+//consle.log(elem);
+//}
